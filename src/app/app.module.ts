709b14aa5ae8697a1d484264bac10a514bec7279
Parker Harrelson <parker.t.harrelson@jbhunt.com>
2021-08-12 22:57:47 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginSignUpComponent } from './login-sign-up/login-sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginSignUpComponent
   ],
   imports: [
     BrowserModule
