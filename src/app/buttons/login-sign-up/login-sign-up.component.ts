d8d6b5ea9021ed091b06c318f7c197df006c3290
Parker Harrelson <parker.t.harrelson@jbhunt.com>
2021-08-17 18:56:08 -0500

    Refactored the sign in button to allow it to quickly navigate to the sign in page instead of having a delay

--- src/app/buttons/login-sign-up/login-sign-up.component.ts
+++ src/app/buttons/login-sign-up/login-sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-sign-up',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginSignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigate(): void {
+
+    this.router.navigateByUrl('/adduser');
   }
 
 }
